544a49e36976d62cfbd1198dee6090fed95092a1
Mr-Fougere <alexandre7822@gmail.com>
2023-11-13 10:46:49 +0100

    steevie bushes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
         debbie_credentials = @session.rabbit_credentials("Debbie")
         @debbie_key = debbie_credentials[:rabbit_key]
         @debbie_uuid = debbie_credentials[:rabbit_uuid]
-
+        @bushes = bush_generator(7)
     end
 
     def start_session
@@ -21,5 +21,20 @@ class ApplicationController < ActionController::Base
 
     def set_session
         @session = Session.find_by(uuid: cookies[:uuid])
+    end
+
+    private 
+
+    def bush_generator(number)
+        positions = number.times.map do |i|
+            {top: rand(10..90), left: rand(10..90)}
+        end
+        steevie_hide = rand(0...number)
+        steevie_credentials = @session.rabbit_credentials("Steevie")
+        steevie_key = steevie_credentials[:rabbit_key]
+        steevie_uuid = steevie_credentials[:rabbit_uuid]
+        positions[steevie_hide][:rabbit_key] = steevie_key
+        positions[steevie_hide][:rabbit_uuid] = steevie_uuid
+        positions
     end
 end
