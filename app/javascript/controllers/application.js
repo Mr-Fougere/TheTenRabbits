65b3c93b331ace342b3177be6978d9d66982eab6
Mr-Fougere <alexandre7822@gmail.com>
2023-11-17 00:57:26 +0100

    fix prod 3

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -11,19 +11,3 @@ export { application };
 window.ondragstart = function () {
   return false;
 };
-
-export function seekRabbit(element) {
-  if (!element.dataset.key && !element.dataset.uuid) return;
-  const requestData = {
-    rabbit_key: element.dataset.key,
-    rabbit_uuid: element.dataset.uuid,
-  };
-
-  return fetch("/seek_rabbit", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(requestData),
-  });
-}
