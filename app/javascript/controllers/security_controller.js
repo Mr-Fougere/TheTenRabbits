760fe7e61006905fae3c916da28f8ca1511c1d42
Mr-Fougere <alexandre7822@gmail.com>
2023-11-24 22:50:50 +0100

    change href security controller

--- app/javascript/controllers/security_controller.js
+++ app/javascript/controllers/security_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller {
   
   connect() {
     setTimeout(() => {
-        window.location.href = "http://localhost:3000";
+        window.location.href = "https://the-ten-rabbits-770a3f6488e7.herokuapp.com";
     }, 5000);
   }
   
